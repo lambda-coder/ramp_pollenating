070232cff887d1fa8a9ac5f0e6ca88a4d243baa7
Patrick Marty <marty.patrick@gmail.com>
2015-10-08 10:35:41 +0200

    model 2

--- classifier.py
+++ classifier.py
@@ -4,8 +4,20 @@ from sklearn.base import BaseEstimator
 import os
 from lasagne import layers, nonlinearities
 from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import NeuralNet, BatchIterator
 import numpy as np
+from nolearn.lasagne.handlers import EarlyStopping
+
+
+class FlipBatchIterator(BatchIterator):
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices, :, ::-1, :]
+        return Xb, yb
+
 
 def build_model(hyper_parameters):
     net = NeuralNet(
@@ -19,6 +31,7 @@ def build_model(hyper_parameters):
             ('pool3', layers.MaxPool2DLayer),
             ('hidden4', layers.DenseLayer),
             ('hidden5', layers.DenseLayer),
+            ('dropout5', layers.DropoutLayer),
             ('output', layers.DenseLayer),
             ],
         input_shape=(None, 3, 64, 64),
@@ -31,12 +44,15 @@ def build_model(hyper_parameters):
 hyper_parameters = dict(
     conv1_num_filters=64, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
     conv2_num_filters=128, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(4, 4),
     hidden4_num_units=500, hidden5_num_units=500,
+    dropout5_p=0.5,
     output_num_units=18, output_nonlinearity=nonlinearities.softmax,
     update_learning_rate=0.01,
     update_momentum=0.9,
-    max_epochs=1,
+    max_epochs=200,
+    on_epoch_finished=[EarlyStopping(patience=20, criterion='valid_accuracy', criterion_smaller_is_better=False)],
+    batch_iterator_train=FlipBatchIterator(batch_size=256)
 )
 
 
